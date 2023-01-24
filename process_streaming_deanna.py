6037d993e3df127aa3c88dfac8481f423293e0d6
Deanna Clayton <d99clayton@gmail.com>
2023-01-23 21:57:42 -0600

    Add new files and task 4 changes

---
+++ process_streaming_deanna.py
@@ -0,0 +1,48 @@
+
+import csv
+import socket
+import time
+
+host = "localhost"
+port = 9999
+address_tuple = (host, port)
+
+# use an enumerated type to set the address family to (IPV4) for internet
+socket_family = socket.AF_INET 
+
+# use an enumerated type to set the socket type to UDP (datagram)
+socket_type = socket.SOCK_DGRAM 
+
+# use the socket constructor to create a socket object we'll call sock
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+# read from a file to get some fake data
+input_file = open("TeamsFranchises.csv", "r")
+output_file = open("out9.txt", "w")
+
+# use the built0in sorted() function to get them in chronological order
+reversed = sorted(input_file)
+
+# create a csv reader for our comma delimited data
+reader = csv.reader(reversed, delimiter=",")
+writer = csv.writer(output_file, delimiter=",")
+
+for row in reader:
+    # read a row from the file
+    FranchiseID, FranchiseName, Status, Association = row
+
+    # use an fstring to create a message from our data
+    # notice the f before the opening quote for our string?
+    fstring_message = f"[{FranchiseID}, {FranchiseName}, {Status}, {Association}]"
+    
+    # prepare a binary (1s and 0s) message to stream
+    MESSAGE = fstring_message.encode()
+
+    # use the socket sendto() method to send the message
+    sock.sendto(MESSAGE, address_tuple)
+    print (f"Sent: {MESSAGE} on port {port}.")
+
+    writer.writerow([FranchiseID, FranchiseName, Status, Association])
+
+    # sleep for a few seconds
+    time.sleep(3)
